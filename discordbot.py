637aec19c7f1b19cfd07987518f34a20b4525fea
rutti-bep <s.konnno.train233@gmail.com>
2020-12-19 02:52:42 +0900

    add embed proto

--- discordbot.py
+++ discordbot.py
@@ -21,5 +21,27 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
+    if message.content == '/daily':
+        embed=discord.Embed(title="デイリー", description="today", color=0x31af4b)
+        embed.add_field(name="樹脂", value="21/160(残り時間:Xmin 満杯HH:MM)", inline=False)
+        embed.add_field(name="任務", value=":white_check_mark:", inline=True)
+        embed.add_field(name="聖遺物拾い", value=":white_check_mark:", inline=True)
+        embed.set_footer(text="更新は5時")
+        await message.channel.send(embed=embed)
+
+    if message.content == '/weekly':
+        embed=discord.Embed(title="ボス", color=0x31af4b)
+        embed.add_field(name="トワリン", value=":x:", inline=True)
+        embed.add_field(name="狼", value=":white_check_mark:", inline=True)
+        embed.add_field(name="タルタル", value=":x:", inline=True)
+        await message.channel.send(embed=embed)
+
+        embed=discord.Embed(title="評判", color=0xc30404)
+        embed.add_field(name="討伐", value="1/3", inline=True)
+        embed.add_field(name="任務", value="3/3", inline=True)
+        await message.channel.send(embed=embed)
+
+        await message.channel.send('*mm/ddまで*')
+
 # Botの起動とDiscordサーバーへの接続
 client.run(settings.DiscordApiKey)
---
+++ genshinNotificationBot.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class embedColor(Enum):
+    RED=0xc30404
+    GREEN=0x31af4b
+
+class genshinNotificationBot:
+    
